64bd5269ceeccba68450d61aa1b3c17df2835d4d
Tanvir Ahmod <tanvirahmodcse@gmail.com>
2020-08-17 17:20:26 +0600

    Initial commit

---
+++ app/src/main/java/com/example/scheduledmessenger/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.scheduledmessenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
