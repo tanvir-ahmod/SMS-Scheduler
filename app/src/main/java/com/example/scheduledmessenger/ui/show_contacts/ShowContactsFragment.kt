0daa2d7237df2c8826d2ce8a088ce810e5aa7026
tanvir-ahmod <tanvirahmodcse@gmail.com>
2021-04-25 13:43:57 +0600

    Update show contacts UI

--- app/src/main/java/com/example/scheduledmessenger/ui/show_contacts/ShowContactsFragment.kt
+++ app/src/main/java/com/example/scheduledmessenger/ui/show_contacts/ShowContactsFragment.kt
@@ -1,20 +1,24 @@
 package com.example.scheduledmessenger.ui.show_contacts
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.SearchView
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.scheduledmessenger.R
 import com.example.scheduledmessenger.base.BaseFragment
 import com.example.scheduledmessenger.databinding.FragmentShowContactsBinding
 import com.example.scheduledmessenger.ui.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
+
 
 @ExperimentalCoroutinesApi
 @FlowPreview
@@ -29,6 +33,12 @@ class ShowContactsFragment : BaseFragment<ShowContactsViewModel, FragmentShowCon
     override fun getViewBinding(): FragmentShowContactsBinding =
         FragmentShowContactsBinding.inflate(layoutInflater)
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true) //Make sure you have this line of code.
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initUI()
@@ -39,21 +49,6 @@ class ShowContactsFragment : BaseFragment<ShowContactsViewModel, FragmentShowCon
             layoutManager = LinearLayoutManager(requireContext())
             adapter = contactsAdapter
         }
-
-        mViewBinding.etSearch.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(p0: Editable?) {
-
-            }
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                mViewModel.getContactsFromText(p0.toString())
-            }
-
-        })
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -75,5 +70,22 @@ class ShowContactsFragment : BaseFragment<ShowContactsViewModel, FragmentShowCon
     override fun onDestroy() {
         hideKeyboard(requireContext(), mViewBinding.root)
         super.onDestroy()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.show_contact_menu, menu)
+        val searchViewItem: MenuItem = menu.findItem(R.id.app_bar_search)
+        val searchView: SearchView = searchViewItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                mViewModel.getContactsFromText(newText.toString())
+                return false
+            }
+        })
+        return super.onCreateOptionsMenu(menu, inflater)
     }
 }
